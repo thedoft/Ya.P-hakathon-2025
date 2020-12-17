1424b9fed2e39bbb62964ca585723fd9ed4752b5
thedoft <thedoft@gmail.com>
2020-12-17 22:43:14 +0300

    feat: react-select component and style it

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,30 +2,30 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer class="footer">
-      <img src="#" alt="#" class="logo" />
-      <div class="copyright">
-        <div class="copyright__sympol">&copy;</div>
-        <div class="copyright__text">2020-2025 ГПИ проект правительства РФ </div>
+    <footer className="footer">
+      <img src="#" alt="#" className="logo" />
+      <div className="copyright">
+        <div className="copyright__sympol">&copy;</div>
+        <div className="copyright__text">2020-2025 ГПИ проект правительства РФ </div>
       </div>
-      <ul class="menu-list">
-        <li class="menu-list__item">
-          <a href="/" class="menu-list__link">Главная</a>
+      <ul className="menu-list">
+        <li className="menu-list__item">
+          <a href="/" className="menu-list__link">Главная</a>
         </li>
-        <li class="menu-list__item">
-          <a href="##" class="menu-list__link">Обращения</a>
+        <li className="menu-list__item">
+          <a href="##" className="menu-list__link">Обращения</a>
         </li>
-        <li class="menu-list__item">
-          <a href="###" class="menu-list__link">Результаты</a>
+        <li className="menu-list__item">
+          <a href="###" className="menu-list__link">Результаты</a>
         </li>
-        <li class="menu-list__item">
-          <a href="####" class="menu-list__link">Поддержка</a>
+        <li className="menu-list__item">
+          <a href="####" className="menu-list__link">Поддержка</a>
         </li>
-        <li class="menu-list__item">
-          <a href="#####" class="menu-list__link">Частые вопросы</a>
+        <li className="menu-list__item">
+          <a href="#####" className="menu-list__link">Частые вопросы</a>
         </li>
-        <li class="menu-list__item">
-          <a href="######" class="menu-list__link">Соцсети</a>
+        <li className="menu-list__item">
+          <a href="######" className="menu-list__link">Соцсети</a>
         </li>
       </ul>
     </footer>
