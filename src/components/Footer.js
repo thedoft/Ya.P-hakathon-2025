b7de6567a558bc303d96bf64661b258e8347ec0a
Roman Grishin <r.grishin@bk.ru>
2020-12-17 11:07:27 +0300

    dev: add fonts add copyright block

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,30 +3,31 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer class="footer">
-    <img src="#" alt="#" class="logo" />
-    <div class="footer__copyright-wrapper">
-
-    </div>
-    <ul class="menu-list">
-      <li class="menu-list__item">
-        <a href="/" class="menu-list__link">Главная</a>
-      </li>
-      <li class="menu-list__item">
-        <a href="##" class="menu-list__link">Обращения</a>
-      </li>
-      <li class="menu-list__item">
-        <a href="###" class="menu-list__link">Результаты</a>
-      </li>
-      <li class="menu-list__item">
-        <a href="####" class="menu-list__link">Поддержка</a>
-      </li>
-      <li class="menu-list__item">
-        <a href="#####" class="menu-list__link">Частые вопросы</a>
-      </li>
-      <li class="menu-list__item">
-        <a href="######" class="menu-list__link">Соцсети</a>
-      </li>
-    </ul>
-  </footer>
+      <img src="#" alt="#" class="logo" />
+      <div class="copyright">
+        <div class="copyright__sympol">&copy;</div>
+        <div class="copyright__text">2020-2025 ГПИ проект правительства РФ </div>
+      </div>
+      <ul class="menu-list">
+        <li class="menu-list__item">
+          <a href="/" class="menu-list__link">Главная</a>
+        </li>
+        <li class="menu-list__item">
+          <a href="##" class="menu-list__link">Обращения</a>
+        </li>
+        <li class="menu-list__item">
+          <a href="###" class="menu-list__link">Результаты</a>
+        </li>
+        <li class="menu-list__item">
+          <a href="####" class="menu-list__link">Поддержка</a>
+        </li>
+        <li class="menu-list__item">
+          <a href="#####" class="menu-list__link">Частые вопросы</a>
+        </li>
+        <li class="menu-list__item">
+          <a href="######" class="menu-list__link">Соцсети</a>
+        </li>
+      </ul>
+    </footer>
   )
 }
